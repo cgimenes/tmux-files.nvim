f3ab6783639359ae88f2f1f167f8d994d193e0ef
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-06 14:40:28 -0300

    Fix enter action and prettier UI

--- lua/tmux-files/init.lua
+++ lua/tmux-files/init.lua
@@ -1,24 +1,45 @@
+local core = require 'fzf-lua.core'
+local defaults = require 'fzf-lua.defaults'
+local config = require 'fzf-lua.config'
+local make_entry = require 'fzf-lua.make_entry'
+
 local M = {}
 
-function system_cmd(cmd, opts)
+local function system_cmd(cmd, opts)
   opts = opts or {}
   local text = vim.system(cmd, { text = true }):wait().stdout
   if opts.text then
     return text
   end
-  lines = {}
+  local lines = {}
   for line in string.gmatch(text, '[^\n]+') do
     table.insert(lines, line)
   end
   return lines
 end
 
-function escape_pattern(text)
+local function escape_pattern(text)
   return text:gsub('([^%w])', '%%%1')
 end
 
 M.select = function()
-  local contents = function(cb)
+  local opts = {}
+  opts = config.normalize_opts(opts, {
+    previewer = defaults._default_previewer_fn,
+    prompt = 'Tmux Files> ',
+    file_icons = true and defaults._has_devicons,
+    color_icons = true,
+    _actions = function()
+      return defaults.globals.actions.files
+    end,
+  })
+  if not opts then
+    return
+  end
+  opts = core.set_header(opts)
+  opts = core.set_fzf_field_index(opts)
+
+  local contents = function(fzf_cb)
     coroutine.wrap(function()
       local co = coroutine.running()
 
@@ -37,54 +58,54 @@ M.select = function()
         end
       end
 
-      local files = {}
+      local hash = {}
       for _, file in ipairs(project_files) do
         local pattern = escape_pattern(file)
+
         for match in string.gmatch(contents, pattern .. ':%d+') do
-          table.insert(files, match)
+          if not hash[match] then
+            hash[match] = true
+
+            local entry = make_entry.file(match, opts)
+            if not entry then
+              -- entry to be skipped (e.g. 'cwd_only')
+              coroutine.resume(co)
+            else
+              fzf_cb(entry, function(err)
+                coroutine.resume(co)
+                if err then
+                  fzf_cb(nil)
+                end
+              end)
+              coroutine.yield()
+            end
+          end
         end
         for match in string.gmatch(contents, pattern) do
-          table.insert(files, match)
+          if not hash[match] then
+            hash[match] = true
+
+            local entry = make_entry.file(match, opts)
+            if not entry then
+              -- entry to be skipped (e.g. 'cwd_only')
+              coroutine.resume(co)
+            else
+              fzf_cb(entry, function(err)
+                coroutine.resume(co)
+                if err then
+                  fzf_cb(nil)
+                end
+              end)
+              coroutine.yield()
+            end
+          end
         end
       end
 
-      local hash = {}
-      for _, entry in ipairs(files) do
-        if not hash[entry] then
-          hash[entry] = true
-          cb(entry, function(err)
-            coroutine.resume(co)
-            if err then
-              cb(nil)
-            end
-          end)
-          coroutine.yield()
-        end
-      end
       -- done
-      cb(nil)
+      fzf_cb(nil)
     end)()
   end
-
-  local core = require 'fzf-lua.core'
-  local defaults = require 'fzf-lua.defaults'
-  local config = require 'fzf-lua.config'
-  local opts = {
-    previewer = defaults._default_previewer_fn,
-    prompt = 'Tmux Files> ',
-    file_icons = true and defaults._has_devicons,
-    color_icons = true,
-    _actions = function()
-      return defaults.globals.actions.files
-    end,
-    _cached_hls = { 'path_linenr' },
-  }
-  opts = config.normalize_opts(opts, {})
-  if not opts then
-    return
-  end
-  opts = core.set_header(opts, { 'cwd' })
-  opts = core.set_fzf_field_index(opts)
   return core.fzf_exec(contents, opts)
 end
 
