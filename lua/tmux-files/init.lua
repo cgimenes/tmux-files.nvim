e2920b5c83a28a06d5b14bbf21620997b41acfa5
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2024-11-22 03:49:10 -0300

    Bringing changes made on my fzf-lua fork

--- lua/tmux-files/init.lua
+++ lua/tmux-files/init.lua
@@ -1,122 +1,47 @@
+local uv = vim.uv or vim.loop
 local core = require 'fzf-lua.core'
-local defaults = require 'fzf-lua.defaults'
+local utils = require 'fzf-lua.utils'
 local config = require 'fzf-lua.config'
 local make_entry = require 'fzf-lua.make_entry'
 
 local M = {}
 
-local function system_cmd(cmd, opts)
-  opts = opts or {}
-  local text = vim.system(cmd, { text = true }):wait().stdout
-  if opts.text then
-    return text
-  end
-  local lines = {}
-  for line in string.gmatch(text, '[^\n]+') do
-    table.insert(lines, line)
-  end
-  return lines
-end
-
-local function escape_pattern(text)
-  return text:gsub('([^%w])', '%%%1')
-end
-
-M.select = function()
-  local opts = {}
+M.select = function(opts)
   opts = config.normalize_opts(opts, {
-    previewer = defaults._default_previewer_fn,
-    prompt = 'Tmux Files> ',
-    file_icons = true and defaults._has_devicons,
-    color_icons = true,
-    _actions = function()
-      return defaults.globals.actions.files
-    end,
+    prompt           = "Tmux Files> ",
+    file_icons       = true and M._has_devicons,
+    color_icons      = true,
+    git_icons        = false,
+    previewer        = M._default_previewer_fn,
+    cwd_only         = true,
+    cwd              = nil,
+    _actions         = function() return M.globals.actions.files end,
+    cmd              = string.gsub([=[tmux list-panes -F "#{pane_id}" | grep -Fvx $TMUX_PANE | xargs -I {} tmux capture-pane -p -t {} -S -10000 | grep -oiE "(^|^\.|[[:space:]]|[[:space:]]\.|[[:space:]]\.\.|^\.\.)[[:alnum:]~_-]*/[][[:alnum:]_.#$%&+=/@-]*(:\d+(:\d+)?)?" | sort -u]=], [[\$TMUX_PANE]], vim.env.TMUX_PANE),
   })
-  if not opts then
-    return
+  if not opts then return end
+
+  opts.fn_transform = function(item)
+    item = vim.trim(item)
+
+    local s = utils.strsplit(item, ":")
+    local filepath = item
+    if #s > 1 then
+      filepath = s[1]
+    end
+    if opts.cwd_only and string.match(filepath, "%.%./") then
+      return nil
+    end
+    if utils.path_is_directory(filepath) then return nil end
+    -- FIFO blocks `fs_open` indefinitely (#908)
+    if utils.file_is_fifo(filepath, uv.fs_stat(filepath)) or not utils.file_is_readable(filepath) then
+      return nil
+    end
+
+    return make_entry.file(item, opts)
   end
-  opts = core.set_header(opts)
+
   opts = core.set_fzf_field_index(opts)
-
-  local contents = function(fzf_cb)
-    coroutine.wrap(function()
-      local co = coroutine.running()
-
-      local panes = system_cmd { 'tmux', 'list-panes', '-F', '#{pane_id}' }
-      local current_pane = system_cmd({ 'sh', '-c', 'echo $TMUX_PANE ' })[1]
-
-      local project_files = system_cmd { 'fd', '--color=never', '--type', 'f', '--hidden', '--follow', '--exclude', '.git' }
-      local num_history_lines = 10000
-
-      local contents = ''
-
-      for _, pane_id in ipairs(panes) do
-        if pane_id ~= current_pane then
-          local content = system_cmd({ 'sh', '-c', 'tmux capture-pane -p -t ' .. pane_id .. ' -S ' .. -num_history_lines }, { text = true })
-          contents = content .. '\n'
-        end
-      end
-
-      local hash = {}
-      for _, file in ipairs(project_files) do
-        local pattern = escape_pattern(file)
-
-        for match in string.gmatch(contents, pattern .. ':%d+') do
-          vim.schedule(function()
-            if not hash[match] then
-              hash[match] = true
-
-              local entry = make_entry.file(match, opts)
-              if not entry then
-                -- entry to be skipped (e.g. 'cwd_only')
-                coroutine.resume(co)
-              else
-                fzf_cb(entry, function(err)
-                  coroutine.resume(co)
-                  if err then
-                    fzf_cb(nil)
-                  end
-                end)
-              end
-            else
-              -- entry skipped
-              coroutine.resume(co)
-            end
-          end)
-          coroutine.yield()
-        end
-        for match in string.gmatch(contents, pattern) do
-          vim.schedule(function()
-            if not hash[match] then
-              hash[match] = true
-
-              local entry = make_entry.file(match, opts)
-              if not entry then
-                -- entry to be skipped (e.g. 'cwd_only')
-                coroutine.resume(co)
-              else
-                fzf_cb(entry, function(err)
-                  coroutine.resume(co)
-                  if err then
-                    fzf_cb(nil)
-                  end
-                end)
-              end
-            else
-              -- entry skipped
-              coroutine.resume(co)
-            end
-          end)
-          coroutine.yield()
-        end
-      end
-
-      -- done
-      fzf_cb(nil)
-    end)()
-  end
-  return core.fzf_exec(contents, opts)
+  core.fzf_exec(opts.cmd, opts)
 end
 
 return M
